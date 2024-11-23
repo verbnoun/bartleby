fd1843fc178128dbf632a134d9706c2d1997b986
Verb / Noun <188970+verbnoun@users.noreply.github.com>
2024-11-23 16:25:26 -0500

    envelope

--- midi.py
+++ midi.py
@@ -9,7 +9,7 @@ from adafruit_midi.control_change import ControlChange
 from adafruit_midi.channel_pressure import ChannelPressure
 
 class Constants:
-    DEBUG = False
+    DEBUG = True
     # MIDI Transport Settings
     MIDI_BAUDRATE = 31250
     UART_TIMEOUT = 0.001
